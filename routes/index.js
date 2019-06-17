8f3b8af76cf0024bb1c43f1fe30ef6b52c9aacd2
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-06-16 22:36:29 -0400

    added projects page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/*About Me page*/
+router.get('/aboutme', function(req, res, next) {
+  res.render('about');
+});
+
+/*projects*/
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+});
+
 module.exports = router;
